9a1256be9f32a5ac88f7023a611b16d79525baa6
Sergey Simakov <SaneniRem@yandex.ru>
2025-04-05 12:11:54 +0300

    добавил логи

--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,17 @@
-from time import sleep
+from time import sleep  
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    encoding="utf-8"
+)
+
+logger = logging.getLogger(__name__)
 
 flag = False
 while flag == False:
-    print("Я работаю")
+    logger.info("Я работаю.")
     sleep(10)
