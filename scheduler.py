ac3579fec7ced3bb2a49ab77cab6b1bece9cba96
Sergey Simakov <SaneniRem@yandex.ru>
2025-04-08 18:25:08 +0300

    разделил клиент и воркер

--- scheduler.py
+++ scheduler.py
@@ -13,12 +13,10 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-class Worker:
+class ApiClient:
     def __init__(self, url):
-        self.is_running = False
-        self.client = httpx.Client()
-        self.seen_runs = set()
         self.url = url
+        self.client = httpx.Client()
 
     def get_runs(self):
         try:
@@ -29,7 +27,14 @@ class Worker:
             return data
         except Exception as e:
             logger.exception("Ошибка при запросе данных")
-            return ({"error": str(e)}), 500
+            return {"error": str(e)}, 500
+
+
+class Worker:
+    def __init__(self, api_client):
+        self.is_running = False
+        self.seen_runs = set()
+        self.api_client = api_client
 
     def get_new_runs(self, data):
         new_run_found = False
@@ -45,7 +50,7 @@ class Worker:
     def start(self):
         self.is_running = True
         while self.is_running:
-            data = self.get_runs()
+            data = self.api_client.get_runs()
             if data:
                 self.get_new_runs(data)
             sleep(30)
@@ -53,8 +58,9 @@ class Worker:
 
 def main():
     url = "http://localhost:8080/api/v1/srv/runs/?after=2025-03-25T14:50:00.000000+00:00&orderby=updated_at"
-    loop = Worker(url)
-    loop.start()
+    api_client = ApiClient(url)
+    worker = Worker(api_client)
+    worker.start()
 
 
 if __name__ == "__main__":
