fa5f14f180ff17d5e6ca01f5129843a3d6dd3986
Sergey Simakov <SaneniRem@yandex.ru>
2025-04-08 19:20:31 +0300

    датакласс для ранов

--- scheduler.py
+++ scheduler.py
@@ -1,7 +1,7 @@
 import httpx
 import logging
 from time import sleep
-import json
+import dataclasses as dc
 
 logging.basicConfig(
     level=logging.INFO,
@@ -11,6 +11,26 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
+
+
+@dc.dataclass
+class Run:
+    run_id: str
+    job_id: int
+    status: str
+    start_time: str
+    created_at: str | None
+    updated_at: str | None
+
+    def __repr__(self):
+        return (f"Run(\n"
+                f"  run_id={self.run_id},\n"
+                f"  job_id={self.job_id},\n"
+                f"  status={self.status},\n"
+                f"  start_time={self.start_time},\n"
+                f"  created_at={self.created_at},\n"
+                f"  updated_at={self.updated_at}\n"
+                f")")
 
 
 class ApiClient:
@@ -24,9 +44,21 @@ class ApiClient:
             response = self.client.get(self.url)
             response.raise_for_status()
             data = response.json()
-            return data
+
+            runs = []
+            for data_run in data:
+                run = Run(
+                    run_id=data_run['run_id'],
+                    job_id=data_run['job_id'],
+                    status=data_run['status'],
+                    start_time=data_run['start_time'],
+                    created_at=data_run['created_at'],
+                    updated_at=data_run['updated_at']
+                )
+                runs.append(run)
+            return runs
         except Exception as e:
-            logger.exception("Ошибка при запросе данных")
+            logger.exception("Ошибка при запросе данных с API")
             return {"error": str(e)}, 500
 
 
@@ -36,13 +68,12 @@ class Worker:
         self.seen_runs = set()
         self.api_client = api_client
 
-    def get_new_runs(self, data):
+    def get_new_runs(self, runs):
         new_run_found = False
-        for run in data:
-            run_id = run.get('run_id')
-            if run_id and run_id not in self.seen_runs:
-                logger.info(f"Новый run: {json.dumps(run, indent=2)}")
-                self.seen_runs.add(run_id)
+        for run in runs:
+            if run.run_id not in self.seen_runs:
+                logger.info(f"Новый run:\n{run}")
+                self.seen_runs.add(run.run_id)
                 new_run_found = True
         if not new_run_found:
             logger.info("Нет новых ранов.")
@@ -50,9 +81,9 @@ class Worker:
     def start(self):
         self.is_running = True
         while self.is_running:
-            data = self.api_client.get_runs()
-            if data:
-                self.get_new_runs(data)
+            runs = self.api_client.get_runs()
+            if runs:
+                self.get_new_runs(runs)
             sleep(30)
 
 
