3e5dbf389853c045b7880fe7eb178e5e5bcdbcbd
Sergey Simakov <SaneniRem@yandex.ru>
2025-04-13 13:04:58 +0300

    переименовал status в externol_status

--- antworker/schemas.py
+++ antworker/schemas.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 class Run(BaseModel):
     run_id: str
     job_id: int
-    status: str
+    externol_status: str
     start_time: str
     created_at: str | None
     updated_at: str | None
